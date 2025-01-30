f231c6eceb0041dfc050ac0aff9d3623fb80d4b0
nguyentrungtinh <trungtinhn300@gmail.com>
2025-01-30 09:17:17 +0700

    Init project e-learning backend API

---
+++ src/test/java/com/TrungTinhBackend/E_Learning_Backend/ELearningBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrungTinhBackend.E_Learning_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
